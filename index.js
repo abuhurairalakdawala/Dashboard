48f0b845302aa7052bb65708f313ef1e142058bd
Abu Huraira Lakdawala <abu2602@gmail.com>
2022-09-11 15:34:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const os = require('node:os'); 
-
-
-console.log(os.version()); 
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json)) 
